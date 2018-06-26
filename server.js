a93bc9bf440837bd8216c60ff4bcbbd4da7002b0
Simon Darmandieu <simon.darmandieu@gmail.com>
2018-06-26 22:57:35 +0200

    POO new user

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ var app = express();
 
 // var {} = require('./utils/utils.js');
 
-// var bodyparser = require('body-parser')
-// app.use(bodyparser.urlencoded({ extended: false }))
+var bodyparser = require('body-parser')
+app.use(bodyparser.urlencoded({ extended: false }))
 
 app.set('view engine','ejs');
 
@@ -16,7 +16,18 @@ app.get('/' , function(req,res){
 
 app.get('/register' , function(req,res){
 	res.render('register.ejs');
+});
+
+app.post('/registered' , function(req,res) {
+	var new_user = new User(req.body.lastname,req.body.firstname,req.body.mail,req.body.password);
+	res.render('register-success.ejs');
+});
+
+app.get('/signin' , function(req,res){
+	res.render('signin.ejs');
 })
+
+
 
 const PORT = process.env.PORT || 8080; 
 
---
+++ utils/utils.js
@@ -0,0 +1,19 @@
+//les machins pour se connecter à la DB , avec l'url etc
+// nom de db etc
+
+exports.User = function (lastname,firstname,mail,password) {
+	this.lastname = lastname;
+	this.firstname = firstname;
+	this.mail = mail;
+	this.password = password;
+	this.register = function(){
+		var currentUser = {
+			lastname : this.lastname,
+			firstname : this.firstname,
+			mail : this.mail,
+			password : this.password
+		};
+		//connexion à la DB
+		// insertion du gars avec la langage SQL ou SQLITE
+	}
+}
