926488a42ba78da240cec98dcec24f4f574cb081
100drine <julien100drine@sfr.fr>
2018-06-27 16:51:24 +0200

    page2

--- server.js
+++ server.js
@@ -33,6 +33,9 @@ app.post('/signedin' , function(req,res){
 	res.render('index.ejs');
 });
 
+app.get('/arbre' , function(req,res){
+    res.render('arbre.ejs');
+});
 
 
 const PORT = process.env.PORT || 8080; 
