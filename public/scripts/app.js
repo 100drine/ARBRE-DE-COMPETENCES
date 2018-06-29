7cca2ad674a1a9d1bf649086845e740a35756cbe
100drine <julien100drine@sfr.fr>
2018-06-29 16:24:01 +0200

    backgrounund

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,10 +2,12 @@ $(document).ready(function(){
     $('.cercle').click(function(){
         $(this).toggleClass ("shad");
     });
-    
-    
-    
-    
-    
-    
+      
+});
+
+$(document).ready(function(){
+    $('.trait').click(function(){
+        $(this).toggleClass ("traitshad");
+        
+    });
 });
